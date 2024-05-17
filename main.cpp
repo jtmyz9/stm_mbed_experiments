908abd8b9c671093b2735f32df29c12d523ce6b2
jmcloud <j123tm@gmail.com>
2024-05-17 13:32:42 -0700

    adding config file

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "mbed.h"
 #include "USBSerial.h"
 
+using namespace std::chrono;
 
 // Blinking rate in milliseconds
 #define BLINKING_RATE     500ms
@@ -14,18 +15,16 @@
 // USB Serial Wrapper
 USBSerial connection;
 // Basic thread to send USB data
-Thread thread;
+Thread usb_thread;
 
 void usb_tx_thread(){
-    ThisThread::sleep_for(5s);
-    connection.disconnect();
-    connection.init();
     connection.connect();
     char hello[100];
-    static uint64_t now = 0;
+    static uint32_t time_since_boot = 0;
+
     while (true) {
-        now = Kernel::get_ms_count();
-        int size = sniprintf(hello, 100, "Hello: Time since boot: %d", now);
+        time_since_boot = duration_cast<std::chrono::milliseconds>(Kernel::Clock::now().time_since_epoch()).count();
+        int size = sniprintf(hello, 100, "Hello: Time since boot: %ld \n", time_since_boot);
 
         connection.send((uint8_t*)hello, size);
         ThisThread::sleep_for(BLINKING_RATE);
@@ -38,7 +37,7 @@ int main()
     // Initialise the digital pin LED1 as an output
     DigitalOut led(LED1);
 
-    thread.start(usb_tx_thread);
+    usb_thread.start(usb_tx_thread);
 
     while (true) {
         led = !led;
